b7d6abac05b4f90cdc22b643d632aa31a887b559
Peter Dimov <pdimov@gmail.com>
2020-09-07 17:38:05 +0300

    Add lambda2_test

---
+++ test/lambda2_test.cpp
@@ -0,0 +1,51 @@
+// Copyright 2020 Peter Dimov
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/lambda2.hpp>
+#include <boost/core/lightweight_test.hpp>
+
+#define TEST_UNARY(Op) \
+    BOOST_TEST_EQ( (Op _1)( 0 ), Op 0 ); \
+    BOOST_TEST_EQ( (Op _1)( 1 ), Op 1 ); \
+    BOOST_TEST_EQ( (Op _1)( 5 ), Op 5 ); \
+    BOOST_TEST_EQ( (Op (Op _1))( 5 ), Op (Op 5) ); \
+    BOOST_TEST_EQ( ((Op _1) + (Op _2))( 1, 2 ), (Op 1) + (Op 2) ); \
+    BOOST_TEST_EQ( ((Op _1) && (Op _2))( 1, 2 ), (Op 1) && (Op 2) );
+
+#define TEST_BINARY(Op) \
+    BOOST_TEST_EQ( (_1 Op _2)( 0, 1 ), 0 Op 1 ); \
+    BOOST_TEST_EQ( (_1 Op _2)( 3, 5 ), 3 Op 5 ); \
+    BOOST_TEST_EQ( (_1 Op 2)( 1, 2 ), 1 Op 2 ); \
+    BOOST_TEST_EQ( (1 Op _2)( 1, 2 ), 1 Op 2 ); \
+    BOOST_TEST_EQ( ((_1 Op _2) + (_3 Op _4))( 1, 2, 3, 4 ), (1 Op 2) + (3 Op 4) );
+
+int main()
+{
+    using namespace boost::lambda2;
+
+    TEST_BINARY(+)
+    TEST_BINARY(-)
+    TEST_BINARY(*)
+    TEST_BINARY(/)
+    TEST_BINARY(%)
+    TEST_UNARY(-)
+
+    TEST_BINARY(==)
+    TEST_BINARY(!=)
+    TEST_BINARY(>)
+    TEST_BINARY(<)
+    TEST_BINARY(>=)
+    TEST_BINARY(<=)
+
+    TEST_BINARY(&&)
+    TEST_BINARY(||)
+    TEST_UNARY(!)
+
+    TEST_BINARY(&)
+    TEST_BINARY(|)
+    TEST_BINARY(^)
+    TEST_UNARY(~)
+
+    return boost::report_errors();
+}
