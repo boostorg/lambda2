5ae7069431851dac264a05da2b47dd71cb478440
Peter Dimov <pdimov@gmail.com>
2021-06-27 05:32:15 +0300

    Add test/placeholders.cpp

---
+++ test/placeholders.cpp
@@ -0,0 +1,29 @@
+// Copyright 2021 Peter Dimov
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/lambda2.hpp>
+#include <boost/core/lightweight_test.hpp>
+#include <functional>
+
+int f( int x )
+{
+    return x;
+}
+
+int main()
+{
+    using namespace boost::lambda2;
+
+    BOOST_TEST_EQ( std::bind(f, _1)( 1 ), 1 );
+    BOOST_TEST_EQ( std::bind(f, _2)( 1, 2 ), 2 );
+    BOOST_TEST_EQ( std::bind(f, _3)( 1, 2, 3 ), 3 );
+    BOOST_TEST_EQ( std::bind(f, _4)( 1, 2, 3, 4 ), 4 );
+    BOOST_TEST_EQ( std::bind(f, _5)( 1, 2, 3, 4, 5 ), 5 );
+    BOOST_TEST_EQ( std::bind(f, _6)( 1, 2, 3, 4, 5, 6 ), 6 );
+    BOOST_TEST_EQ( std::bind(f, _7)( 1, 2, 3, 4, 5, 6, 7 ), 7 );
+    BOOST_TEST_EQ( std::bind(f, _8)( 1, 2, 3, 4, 5, 6, 7, 8 ), 8 );
+    BOOST_TEST_EQ( std::bind(f, _9)( 1, 2, 3, 4, 5, 6, 7, 8, 9 ), 9 );
+
+    return boost::report_errors();
+}
